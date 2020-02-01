831c7ee3b0509755f808600c74c1a361fd48a9eb
reddyse <reddyse@oclc.org>
2020-02-01 11:17:23 -0500

    testing with other person push

--- java/src/com/rupa/oops/InheritanceParent.java
+++ java/src/com/rupa/oops/InheritanceParent.java
@@ -3,7 +3,7 @@ package com.rupa.oops;
 public class InheritanceParent {
 
 	public static void main(String[] args) {
-		System.out.println("inheritance concept");
+		System.out.println("mumu inheritance concept");
 	}
 
 }
