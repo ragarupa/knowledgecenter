9cf9f8f36b68cf7dab18f186a5d194664b92a033
ragarupa <ragarupadevi.12@gmail.com>
2020-02-09 22:15:39 +0530

    unadded

--- java/src/com/rupa/oops/InheritanceParent.java
+++ java/src/com/rupa/oops/InheritanceParent.java
@@ -1,9 +1,15 @@
 package com.rupa.oops;
 
-public class InheritanceParent {
-
-	public static void main(String[] args) {
-		System.out.println("mumu inheritance concept");
+public class InheritanceParent extends InheritanceSuper{
+	
+	protected static void fatherName(String fname, String gfname) {
+		System.out.println("My father's name is:" + fname);
+		//System.out.println("My grandfather's name is:" + super.grandFather(gfname));
 	}
-
+	
+	public String motherName(String mname, String gname) {
+		mname = "My mother's name is:" + mname;
+		System.out.println("My grandmother's name is:" + super.grandMother(gname));
+		return mname;
+	}
 }
